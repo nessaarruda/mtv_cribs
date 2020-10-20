ec527b44bd16fb283f9784f6fda5a63e9a6c8d7a
Vanessa Arruda <wanessaarruda@gmail.com>
2020-10-20 09:15:25 -0600

    Add no renter method

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -3,12 +3,14 @@ class Apartment
   attr_reader :number,
               :monthly_rent,
               :bathrooms,
-              :bedrooms
-              
+              :bedrooms,
+              :renter
+
   def initialize(number: number, monthly_rent: 0, bathrooms: 0, bedrooms: 0)
     @number = number
     @monthly_rent = monthly_rent
     @bathrooms = bathrooms
     @bedrooms = bedrooms
+    @renter = renter
   end
 end
