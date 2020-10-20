fbeec03894cdf3b02fa414ecf4b21d5d32a56d0e
Vanessa Arruda <wanessaarruda@gmail.com>
2020-10-20 09:22:48 -0600

    Add add units test

--- test/building_test.rb
+++ test/building_test.rb
@@ -8,9 +8,15 @@ class BuildingTest < Minitest::Test
 
   def setup
     @building = Building.new
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
   end
 
   def test_units_start_empty
     assert_equal [], @building.units
+  end
+
+  def test_it_can_add_units
+    assert_equal [@unit1, @unit2], @building.units
   end
 end
