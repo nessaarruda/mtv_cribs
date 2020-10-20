d0d7d403f368f4c030583009aa371c1a7c9e4cba
Vanessa Arruda <wanessaarruda@gmail.com>
2020-10-20 09:06:01 -0600

    Create renter test for attributes and all relevant files

---
+++ test/renter_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+require './lib/apartment'
+
+class RenterTest < Minitest::Test
+
+  def setup
+    @renter1 = Renter.new("Jessie")
+  end
+  
+  def test_it_exists_and_has_attributes
+    assert_equal "Jessie", @renter1.name
+  end
+end
